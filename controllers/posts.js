409c7779b7774debe20c66b594f9b9350cccf72d
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2022-05-30 20:08:13 +0500

    Update and delete functionality

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,5 @@
 import PostMessage from "../models/postMessage.js";
+import mongoose from "mongoose";
 
 export const getPosts = async (req, res) => {
   try {
@@ -15,8 +16,33 @@ export const createPost = async (req, res) => {
 
   try {
     await newPost.save();
-    res.status(201).json();
+    res.status(201).json(newPost);
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
+};
+
+export const updatePost = async (req, res) => {
+  const { id } = req.params;
+  const { title, message, creator, selectedFile, tags } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id: ${id}`);
+
+  const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+
+  await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+
+  res.json(updatedPost);
+};
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send(`No post with id: ${id}`);
+  }
+
+  await PostMessage.findByIdAndRemove(id);
+  res.json({ message: "Post deleted Successfully" });
 };
