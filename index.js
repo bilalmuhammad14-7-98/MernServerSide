6d5719ba40ebae6de7bf8d18c865e8b329e434da
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2022-05-31 18:06:29 +0500

    Like functionality

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "  mongodb+srv://mbilal:abc123+-@cluster0.itrdu.mongodb.net/?retryWrites=true&w=majority";
+const CONNECTION_URL = process.env.CONNECTION_URL;
+// "  mongodb+srv://mbilal:abc123+-@cluster0.itrdu.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
